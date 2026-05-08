592b52142450323b940f32b26f121e3b5ea0fdee
Tohru Yaginuma <toru45s@gmail.com>
2026-05-08 00:36:21 -0700

    update models

--- product_catalog/product/models.py
+++ product_catalog/product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
